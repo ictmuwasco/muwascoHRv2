e331cd71930b608a4b129fd2387440f8da72a7ce
ictmuwasco <ict@uwasco.co.ke>
2026-08-03 12:58:39 +0300

    Add missing Employee and Department repository/service bindings to AppServiceProvider

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -13,11 +13,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->bind(\App\Repositories\Contracts\EmployeeRepositoryInterface::class, \App\Repositories\EmployeeRepository::class);
+        $this->app->bind(\App\Repositories\Contracts\DepartmentRepositoryInterface::class, \App\Repositories\DepartmentRepository::class);
         $this->app->bind(\App\Repositories\Contracts\SectionRepositoryInterface::class, \App\Repositories\SectionRepository::class);
         $this->app->bind(\App\Repositories\Contracts\AttendanceRepositoryInterface::class, \App\Repositories\AttendanceRepository::class);
         $this->app->bind(\App\Repositories\Contracts\LeaveRepositoryInterface::class, \App\Repositories\LeaveRepository::class);
         $this->app->bind(\App\Repositories\Contracts\UserRepositoryInterface::class, \App\Repositories\UserRepository::class);
 
+        $this->app->bind(\App\Services\Contracts\EmployeeServiceInterface::class, \App\Services\EmployeeService::class);
+        $this->app->bind(\App\Services\Contracts\DepartmentServiceInterface::class, \App\Services\DepartmentService::class);
         $this->app->bind(\App\Services\Contracts\SectionServiceInterface::class, \App\Services\SectionService::class);
         $this->app->bind(\App\Services\Contracts\AttendanceServiceInterface::class, \App\Services\AttendanceService::class);
         $this->app->bind(\App\Services\Contracts\LeaveServiceInterface::class, \App\Services\LeaveService::class);
